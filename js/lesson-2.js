fc0a698f64679f0898cf1098191027d425faf128
Yurii <oniks9821@gmail.com>
2024-09-14 15:28:29 +0300

    task2.2

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -25,16 +25,52 @@
 // якщо є - виводить повідомлення в alert "Welcome, <name>!"
 // в іншому випадку - "User not found"
 
-const logins = ["Peter", "John", "Igor", "Sasha"];
-function checkLogin(array) {
-    const login = prompt("Enter your login");
-    // for (const element of array) {
-    //     if (login === element) {
-    //         alert(`Welcome, ${login}!`);
-    //         return;
-    //     }; 
-    // };
-    // alert("User not found");
-    alert(array.includes(login) ? `Welcome, ${login}!` : "User not found");
+// const logins = ["Peter", "John", "Igor", "Sasha"];
+// function checkLogin(array) {
+//     const login = prompt("Enter your login");
+//     // for (const element of array) {
+//     //     if (login === element) {
+//     //         alert(`Welcome, ${login}!`);
+//     //         return;
+//     //     };
+//     // };
+//     // alert("User not found");
+//     alert(array.includes(login) ? `Welcome, ${login}!` : "User not found");
+// }
+// checkLogin(logins);
+
+
+
+// 3. Напишіть функцію, яка складатиме сусідні числа і пушитиме їх в новий масив
+const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+
+
+// function sum(array) {
+//     const sumArray = [];
+//     for (let i = 0; i < array.length-1; i++){
+//         let current = array[i];
+//         let next = array[i + 1];
+//         sumArray.push(current + next);
+//     }
+//     return sumArray;
+// }
+
+// console.log(sum(someArr));
+
+
+// 5. Напишіть функцію, яка шукатиме найдовше слово у реченні
+console.log(findLongestWord("London is the capital of Great Britain")); // 'capital'
+
+
+function findLongestWord(str) {
+    const array = str.split(" ");
+    let longestWord = array[0];
+    for (const word of array) {
+        if (longestWord.length < word.length) {
+            longestWord = word;
+           
+        }
+         console.log(longestWord);
+    }
+    return longestWord;
 }
-checkLogin(logins);
